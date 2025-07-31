cbd93d736c7a29e38e606bfdfeaf53abf4593244
seet <jeffseet@gmail.com>
2025-07-31 10:57:48 +0800

    (feat) Update App.java with logger instead of System.println

--- src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
+++ src/main/java/com/sddevops/jenkins_maven/eclipse/App.java
@@ -1,12 +1,17 @@
 package com.sddevops.jenkins_maven.eclipse;
 
+import java.util.logging.Logger; // <-- Required import
+
 /**
  * Hello world!
  */
 public class App {
+	// Create a Logger instance
+	private static final Logger logger = Logger.getLogger(App.class.getName());
+
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("Test");
+		logger.info("Hello World!"); // Replaces System.out.println
+		logger.info("Test"); // Replaces System.out.println
 	}
 
 	// Trigger CI build test
